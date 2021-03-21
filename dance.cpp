a888ed046d09cd08ef39f80fd52bcda59a46da5c
Anox3D <71902913+Anox3D@users.noreply.github.com>
2021-03-22 00:34:44 +0100

    Added dance.cpp so the code

---
+++ dance.cpp
@@ -0,0 +1,91 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <SoftwareSerial.h>
+
+#include <MeMCore.h>
+
+MeDCMotor motor_9(9);
+MeDCMotor motor_10(10);		
+
+void move(int direction, int speed)
+{
+      int leftSpeed = 0;
+      int rightSpeed = 0;
+      if(direction == 1){
+        	leftSpeed = speed;
+        	rightSpeed = speed;
+      }else if(direction == 2){
+        	leftSpeed = -speed;
+        	rightSpeed = -speed;
+      }else if(direction == 3){
+        	leftSpeed = -speed;
+        	rightSpeed = speed;
+      }else if(direction == 4){
+        	leftSpeed = speed;
+        	rightSpeed = -speed;
+      }
+      motor_9.run((9)==M1?-(leftSpeed):(leftSpeed));
+      motor_10.run((10)==M1?-(rightSpeed):(rightSpeed));
+}
+				
+double angle_rad = PI/180.0;
+double angle_deg = 180.0/PI;
+
+
+
+void setup(){
+  // Little dance
+    move(1,255);
+    _delay(1);
+    move(2, 255);
+    _delay(1);
+    move(4,100);
+    _delay(2);
+    move(3,100);
+    _delay(4);
+    move(4, 100);
+    _delay(2);
+  // Little music
+    for(int __i__=0;__i__<2;++__i__)
+    {
+      buzzer.tone(247,500);  // B3
+      delay(10);
+      buzzer.tone(247,500);  // B3
+      delay(10);*
+      buzzer.tone(193,500);  // G3
+      delay(10);
+      buzzer.tone(193,500);  // G3
+      delay(10);
+      buzzer.tone(175,500);  // F3
+      delay(10);
+      buzzer.tone(175,500);  // F3
+      delay(10);
+      buzzer.tone(193,500);  // G3
+      delay(7);
+      buzzer.tone(193,500);  // G3
+      delay(2);
+      buzzer.tone(220,500);  // A3
+      delay(7);
+      buzzer.tone(220,500);  // A3
+      delay(2);
+    }
+  // 360 (or not I don't know, I'll see)
+    move(4,255);
+    _delay(12);
+}
+
+void loop(){
+    
+    
+    _loop();
+}
+
+void _delay(float seconds){
+    long endTime = millis() + seconds * 1000;
+    while(millis() < endTime)_loop();
+}
+
+void _loop(){
+    
+}
+
